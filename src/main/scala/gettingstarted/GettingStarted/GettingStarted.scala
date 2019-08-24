932bf5b51f9527a778bf8ef7ee9bcc3fba067a1a
b-i-hide <b.i1993patriots@gmail.com>
2019-08-25 01:17:19 +0900

    [update] fibのexercise

--- src/main/scala/gettingstarted/GettingStarted/GettingStarted.scala
+++ src/main/scala/gettingstarted/GettingStarted/GettingStarted.scala
@@ -8,4 +8,38 @@ class GettingStarted {
     }
     loop(n, 0, 1)
   }
+
+  def factorial(n: Int): Int = {
+    def go(n: Int, acc: Int): Int =
+      if (n <= 0) acc
+      else go(n - 1, n * acc)
+
+    go(n, 1)
+  }
+}
+
+object MyModile {
+  def abs(n: Int): Int =
+    if (n < 0) -n
+    else n
+
+  private def formatAbs(x: Int) = {
+    val msg = "The absolute value of %d is %d"
+    msg.format(x, abs(x))
+  }
+
+  def main(args: Array[String]): Unit = {}
+
+  private def fib(n: Int): Int = {
+    val n0 = 0
+    val n1 = 1
+
+    def loop(n: Int, x: Int, y: Int): Int = {
+      if (n == 0) x
+      else {
+        loop(n - 1, y, x + y)
+      }
+    }
+    loop(n, n0, n1)
+  }
 }
